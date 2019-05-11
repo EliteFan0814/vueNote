ab9e29eaf15f7052653fc5a3019826b866e6ff52
EliteFan <fan552426811@outlook.com>
2019-05-11 21:11:02 +0800

    vue-cli

---
+++ my-project/src/components/list.vue
@@ -0,0 +1,12 @@
+<template>
+    <ul>
+        <li><router-link to='/'>helloWorld</router-link></li>
+        <li><router-link to='helloEarth'>helloEarth</router-link></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
